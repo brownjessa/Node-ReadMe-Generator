c16c8853fbc4878393fdad6b47d3d8095fc2c653
Jessamine Brown <brownjessa11@gmail.com>
2020-08-25 11:06:02 -0500

    outline questions for info readme

--- index.js
+++ index.js
@@ -2,3 +2,50 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const questions = [
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+  {
+    type: "",
+    name: "",
+    message: "",
+  },
+];
