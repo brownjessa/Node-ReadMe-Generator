6c8bd2cb7eba37efdf43dfdf7bac7f380045096c
Jessamine Brown <brownjessa11@gmail.com>
2020-08-25 11:21:39 -0500

    fixing file location

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
